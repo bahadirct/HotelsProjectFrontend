0c117760807ef366daeb111cbf90f9dc081c9240
Bahadır can Topçu <bahadircantopcu@gmail.com>
2022-07-15 00:15:22 +0300

    Servislerde yazdığım metodları kullandım. -Servislerde yazılan metodları kullandım ve bunun yanında .html tarafında kullanıcıyı apiler ile backend ile etkilşime geçirdim.

--- src/app/components/hotel/hotel.component.ts
+++ src/app/components/hotel/hotel.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,NgModule, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { debounceTime } from 'rxjs';
+import { Hotel } from 'src/app/models/hotel';
+import { HotelService } from 'src/app/Services/hotel.service';
 
 @Component({
   selector: 'app-hotel',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelComponent implements OnInit {
 
-  constructor() { }
+  hotels: Hotel[] = [];
+  loading: boolean = false;
+  file? : File;
+  dataLoaded= false;
+  constructor(private hotelService:HotelService,private toastrService:ToastrService) { }
 
   ngOnInit(): void {
+  this.getHotels();
   }
 
+  getHotels() {
+    this.hotelService.getHotels()
+    .subscribe(response=>{
+      this.hotels = response.data
+      this.dataLoaded=true;
+    })
+  }
+
+public WriteAsJson(){
+  this.hotelService.WriteAsJson().subscribe(response => {
+    this.toastrService.success(response.message);
+  })}
+
+public WriteAsXml(){
+  this.hotelService.WriteAsXml().subscribe(response => {
+    this.toastrService.success(response.message);
+  })}
+
+  public onChange(event : any) {
+    this.file = event.target.files[0];
 }
+
+// OnClick of button Upload
+public  UploadCsv() {
+  this.loading = !this.loading;
+  console.log(this.file);
+  this.hotelService.UploadCsv(this.file!).subscribe(
+      (event: any) => {
+          if (typeof (event) === 'object') {
+
+              // Short link via api response
+              //this.shortLink = event.link;
+              this.toastrService.success(event.message);
+              this.getHotels();
+              this.loading = false; // Flag variable 
+          }
+      }
+  );
+}
+  }
+
+  
+
+    
+  
+
+
+
--- src/app/models/HotelResponseModel.ts
+++
@@ -1,7 +0,0 @@
-import { Hotel } from "./hotel";
-import { ResponseModel } from "./responseModel";
-
-export interface ProductResponseModel extends ResponseModel{
-    data:Hotel[]
-   
-}
