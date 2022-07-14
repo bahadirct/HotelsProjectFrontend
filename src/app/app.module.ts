cd6c6c958f3ff802928d9830653db2ef26b865be
Bahadır can Topçu <bahadircantopcu@gmail.com>
2022-07-15 00:10:54 +0300

    Kullandığım modülleri projeye tanıttım.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HotelComponent } from './components/hotel/hotel.component';
 import { NavibarComponent } from './components/navibar/navibar.component';
+import { CommonModule } from '@angular/common';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     HotelComponent,
-    NavibarComponent
+    NavibarComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({positionClass:"toast-bottom-right"})
   ],
   providers: [],
   bootstrap: [AppComponent]
